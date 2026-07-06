26c3a69ec2981cebfb832a7fefc7972662e9c96a
PRAJAPATI ARYANKUMAR PRAKASHBHAI <prajapatiaryankumar964@gmail.com>
2026-07-06 07:25:33 +0530

    found word in string

---
+++ foundstringdemo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class foundstringdemo
+{
+    public static void main(String args[])
+    {
+        String str = "Java Developer";
+
+        System.out.println(str.contains("Java"));
+    }
+}
