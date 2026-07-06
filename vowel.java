f6751a7072ae53bba0156b02f107c18fab344d56
PRAJAPATI ARYANKUMAR PRAKASHBHAI <prajapatiaryankumar964@gmail.com>
2026-07-06 07:20:53 +0530

    vowel

---
+++ vowel.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class vowel
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("choice: ");
+        char ch = sc.next().toLowerCase().charAt(0);
+
+        switch(ch)
+        {
+            case 'a':
+                System.out.println("Vowel");
+                break;
+
+            case 'e':
+                System.out.println("Vowel");
+                break;
+
+            case 'i':
+                System.out.println("Vowel");
+                break;
+            case 'o':
+                System.out.println("Vowel");
+                break;
+            case 'u':
+                System.out.println("Vowel");
+                break;
+
+            default:
+                System.out.println("Consonant");
+        }
+    }
+}
