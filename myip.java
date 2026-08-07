d11bcfb146acbabd561ea9d8271e406a0528ad73
PRAJAPATI ARYANKUMAR PRAKASHBHAI <prajapatiaryankumar964@gmail.com>
2026-08-07 07:45:21 +0530

    My(pc) IP address Found

---
+++ myip.java
@@ -0,0 +1,16 @@
+import java.net.InetAddress;
+
+public class myip
+{
+    public static void main(String[] args) {
+        try {
+            InetAddress ip = InetAddress.getLocalHost();
+
+            System.out.println("Host Name : " + ip.getHostName());
+            System.out.println("IP Address: " + ip.getHostAddress());
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
