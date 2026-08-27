5749e1890607415a58cbe5f6369e647142b58cee
PRAJAPATI ARYANKUMAR PRAKASHBHAI <prajapatiaryankumar964@gmail.com>
2026-08-27 10:49:25 +0530

    alphabet found

---
+++ substringdemo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class substringdemo
+{
+    public static void main(String args[])
+    {
+        String name = "Aryan";
+
+        System.out.println(name.substring(4));
+    }
+}
