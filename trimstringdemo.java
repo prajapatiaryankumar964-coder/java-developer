33609e267baf32bbec4b0f4ec7c2546e9b217752
ARYANKUMAR PRAKASHBHAI PRAJAPATI <prajapatiaryankumar964@gmail.com>
2026-09-16 08:57:39 +0530

    Add files via upload

---
+++ trimstringdemo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class trimstringdemo
+{
+    public static void main(String args[])
+    {
+        String str = "   Aryan   ";
+
+        System.out.println(str.trim());
+    }
+}
